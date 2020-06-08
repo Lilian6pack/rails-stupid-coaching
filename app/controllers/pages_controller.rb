0eb4d5050af4c93af134a7e8fc586d079493f263
Lilian Besse <lilianbesse31@gmail.com>
2020-06-08 14:22:47 +0200

    fini

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+    def ask
+    end
+
+    def answer
+        if params[:question] == "I am going to work"
+            return @answer = "Great"
+        elsif params[:question].include?("?")
+            return @answer = "Silly question, get dressed and go to work!"
+        else
+            return @answer = "I don't care, get dressed and go to work!"
+        end
+    end
+end
